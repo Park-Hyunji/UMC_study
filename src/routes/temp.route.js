453abe974bd8ec94133b9f82dccf34819ea00dd6
박현지 <119161420+Park-Hyunji@users.noreply.github.com>
2023-11-12 09:42:32 +0900

    week8

---
+++ src/routes/temp.route.js
@@ -0,0 +1,7 @@
+// temp.route.js
+import express from 'express';
+import { tempException } from '../controllers/temp.controller.js';
+
+export const tempRouter = express.Router();
+
+tempRouter.get('/exception/:flag',tempException);
