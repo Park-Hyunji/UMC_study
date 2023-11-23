0275008a10810f498d5fa1682fba081cf8b80443
Park-Hyunji <phj9573@naver.com>
2023-11-24 01:02:58 +0900

    #1

---
+++ src/providers/store.provider.js
@@ -0,0 +1,9 @@
+// store.provider.js
+import { BaseError } from "../../config/error.js";
+import { status } from "../../config/response.status.js";
+import { getPreviewReview } from "../models/store.dao.js";
+
+export const getReview = async (storeId, query) => {
+    const {reviewId, size = 3} = query;
+    return previewReviewResponseDTO(await getPreviewReview(reviewId, size, storeId));
+}
